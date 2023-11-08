498f921184f0760c274561c0d8af71979f24a1dc
Danny Ferney Ardila Ardila <102764977+dannyardila@users.noreply.github.com>
2023-11-08 17:23:50 -0500

    Add files via upload
    proyecto ultima

---
+++ registro/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Registro</title>
+
+</head>
+<body>
+<form method=post>
+
+    <h2>Hola Bienvenido a DX10</h2>
+    <P>Si Quieres ser parte de esta familia registrate</P>
+
+    <div class="input-wrapper">
+        <input type="text" name="name" placeholder="Nombre">
+       
+
+    </div>
+
+    <div class="input-wrapper">
+        <input type="text" name="email" placeholder="Email">
+        
+        
+    </div>
+
+    <div class="input-wrapper">
+        <input type="text" name="direction" placeholder="Direccion">
+       
+    </div>
+
+    <div class="input-wrapper">
+        <input type="tel" name="phone" placeholder="Telefono">
+        
+     </div>
+
+    <div class="input-wrapper">
+        <input type="password" name="password" placeholder="Contraseña">
+       
+        </div>
+
+        <input class="btn" type="submit" name="register" value="Enviar">
+</form>
+    
+</body>
+</html>
